9eb20a2fccfca74f40c1a164aed23ca23f4fc88d
MsheshKumar@199 <maheshkumar25587600@gmail.com>
2021-11-24 18:07:30 +0530

    initial commit

---
+++ src/Components/News.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
+
+export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=41b33c6abebb4a60a8340b82ddfb36c8";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+  }
+  render() {
+    return (
+      <div className="container my-3">
+        <h1> DApes-Top Heading of the Day</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItems
+                  title={element.title ? element.title : ""}
+                  description={element.description}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
