e495669bc9704c0930ee75abdb217b1456d330b1
MsheshKumar@199 <maheshkumar25587600@gmail.com>
2021-11-27 16:14:59 +0530

    updat changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import React, { Component } from 'react'
-import News from './Components/News';
 
-export default class App extends Component {
-  render() {
+import React, { useState } from 'react'
+import NavBar from './components/NavBar';
+import Alert from './components/Alert';
+import News from './components/News';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+
+// function App() {
+  // const [mode, setMode] = useState("light"); //whether the dark mode is enable or not
+  // const [alert, setAlert] = useState(null);
+
+  // const showAlert = (message, type) => {
+  //   setAlert({
+  //     msg: message,
+  //     type: type,
+  //   });
+  //   setTimeout(() => {
+  //     setAlert(null);
+  //   }, 1200);
+  // }
+  const App = ()=> {
+  const pageSize = 5;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  const [progress, setProgress] = useState(0)
+
+
+  // const toggleMode=()=>{
+  //   if(mode=="light"){
+  //     setMode("dark");
+  //     document.body.style.background = "black";
+  //     showAlert(" Dark Mode has been enabled", "success");
+  //     document.title = "TextUtils-Dark Mode";
+  //   }
+  //   else
+  //   {
+  //     setMode("light");
+  //     document.body.style.background = "white";
+  //     // showAlert("Light Mode has been enabled", "success");
+  //     // document.title = "TextUtils-Light Mode";
+  //   }
+  // };
+  // console.log("Mode enabled");
+ 
     return (
       <div>
-        <Navbar/>
-        <News/>
+        <Router>
+        <NavBar/> 
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={progress} 
+      />
+      <Alert alert={alert}/>
+        <Switch>
+          <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
+          <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></Route> 
+          <Route exact path="/general"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
+          <Route exact path="/health"><News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/></Route> 
+          <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/></Route> 
+          <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/></Route> 
+          <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/></Route> 
+        </Switch>
+        </Router>
       </div>
     )
-  }
+ 
 }
 
+export default App;
